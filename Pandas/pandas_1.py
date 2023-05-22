ed75f900449c1935d17624662c9c97215597d431
Reshmi Krishna <reshmimrk@gmail.com>
2023-05-22 16:25:12 +0530

    pandas path modified

--- Pandas/pandas_1.py
+++ Pandas/pandas_1.py
@@ -1,6 +1,16 @@
 import pandas as pd
-path="Pandas\marks_1.csv"
-df = pd.read_csv(path, header=None, sep='|')
+# path="Pandas\marks_1.csv"
+# df = pd.read_csv(path, header=None, sep='|')
+
+# import numpy as np
+# import pandas as pd
+
+# The file is stored at the following path:
+# 'https://media-doselect.s3.amazonaws.com/generic/A08MajL8qN4rq72EpVJbAP1Rw/marks_1.csv'
+# Provide your answer below
+df = pd.read_csv('https://media-doselect.s3.amazonaws.com/generic/A08MajL8qN4rq72EpVJbAP1Rw/marks_1.csv', header=None, sep = '|')# Write your answer here
+
+# print(df)
 
 print(df)
 
--- Pandas/pandas_2.py
+++ Pandas/pandas_2.py
@@ -1,6 +1,7 @@
 import pandas as pd
-path="Pandas\marks_1.csv"
-df = pd.read_csv(path, header=None, sep='|')
+df = pd.read_csv('https://media-doselect.s3.amazonaws.com/generic/A08MajL8qN4rq72EpVJbAP1Rw/marks_1.csv', header=None, sep = '|')# Write your answer here
+
+# df = pd.read_csv(path, header=None, sep='|')
 
 print(df)
 
