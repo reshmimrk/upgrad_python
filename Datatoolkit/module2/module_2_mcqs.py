45f81ef847d6b453e5dfb1cd7ded9b71569839a3
Reshmi Krishna <reshmimrk@gmail.com>
2023-05-22 14:40:43 +0530

    datatoolkit module 2

---
+++ Datatoolkit/module2/module_2_mcqs.py
@@ -0,0 +1,28 @@
+print( ((500//7) % 5) ** 3)
+
+
+S = "I love Python"
+
+print(S[2:6])
+print(S[-11:-7])
+
+# print(S[-11:-8])
+
+print(3 * 3 ** 3)
+
+T = (3, 5, 7, 11)
+# print(T.append(9))   Error
+print("__________")
+L1 = ['Vikas', 'Akshay', 'Sanskar', 'Mahima']
+print(L1[1][-1])
+
+l = [32, 34, 12, 27, 33]
+l.append([14, 19])
+print(len(l)) #6
+
+
+D = {1:['Raj', 22], 2:['Simran', 21], 3:['Rahul', 40]}
+for val in D:
+     print(val)
+
+print(list(range(10, 1, -1)))
